322b7802cd973ad1968657f466164f14bc5bb353
Porson <porsonlee@gmail.com>
2021-11-23 15:07:06 -0800

    updated App by rendering out the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import PlayerList from './components/PlayerList'
+import MatchList from './components/MatchList'
 
 function App() {
   return (
     <div className="App">
-      hello world.
+      <h1>
+        Tourney Matches {" "}
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
