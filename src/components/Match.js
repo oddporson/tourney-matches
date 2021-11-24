408d98971330e4b1c96ef21b8bb8fc20a916cfd3
Porson <porsonlee@gmail.com>
2021-11-23 20:13:45 -0800

    prefer ternary operator over short circuiting && for now

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,19 +1,15 @@
 import React from 'react'
 
-function Match () {
+function Match (props) {
+  // console.log(props)
+  // console.log(props.players[0])
+  const { players, winner, scoreDifference } = props
   return (
     <article className="Match">
-      <h1>
-        Joe <span>vs</span> Bob
-      </h1>
-      {/* To be shown when there is a winner */}
-      <h2>
-        Joe is the winner by 1!
-      </h2>
-      {/* To be shown when there is no winner */}
-      <h2>
-        No winners yet!
-      </h2>
+       <h1>{players[0]} <span>vs</span> {players[1]}</h1>
+      {{winner} ? <h2>{winner} is the winner by {scoreDifference}!</h2> : <h2>No winners yet!</h2>}
+        {/* {winner && <h2>{winner} is the winner by {scoreDifference}!</h2>}
+        {!winner && <h2>No winners yet!</h2>} */}
   </article>
   )
 }
--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -4,11 +4,13 @@ import Match from './Match'
 
 
 function MatchList(props) {
+  const oneMatch = matchData[0];
+  console.log("first Match", oneMatch)
   return(
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
       {/* Matches will be shown here */}
-      <Match />
+      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
     </section>
   )
 }
