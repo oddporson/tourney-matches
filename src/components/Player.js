e30278f1fae994180f005815b4e562efbc66823f
Porson <porsonlee@gmail.com>
2021-11-23 20:35:42 -0800

    pass in the playerHelpers data into Player component and render them with conditional statement using short circuiting

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 
-function Player () {
+function Player (props) {
+  console.log(props)
+  const { firstName, gamerTag, lastName, wins } = props
   return(
     <article className="Player">
     <h1>
-      John <span>Secret</span> Doe
+      {firstName} <span>{gamerTag}</span> {lastName}
     </h1>
+
     {/* To be shown when there is no wins */}
-    <h2 className="zero">Currently with no wins :(</h2>
+    {wins === 0 && <h2 className="zero">Currently with no wins :(</h2>}
     {/* To be shown when there is 1 win */}
-    <h2>Currently at 1 win</h2>
+    {wins === 1 && <h2>Currently at {wins} win</h2>}
     {/* To be shown when there is more than one win */}
-    <h2>Currently at 1+ wins</h2>
+    {wins > 1 &&  <h2>Currently at {wins}+ wins</h2>}
+   
   </article>
   )
 }
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -8,10 +8,14 @@ import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers'
 function PlayerList(props) {
   const playerDataArray = preparePlayerData(playerData);
   const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  // console.log(playerDataArray[0]);
+  const playerOne = parsedPlayerData[0]
+  console.log(playerOne)
+
   return(
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player />
+      <Player firstName={playerOne.firstName} gamerTag = {playerOne.gamerTag} lastName={playerOne.lastName} wins={playerOne.wins} />
     </section>
   )
 }
