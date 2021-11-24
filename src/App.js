ec85557849e791c39f7a6affd4ece51000c820e8
Porson <porsonlee@gmail.com>
2021-11-23 22:13:33 -0800

    Import the playerHelper functions in the App component. Move the processing logic from PlayerList to the App. Update the props given to PlayerList so that it only takes in our processed data. Fix any issues inside the PlayerList component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import MatchList from './components/MatchList'
 import matchData from './data/matchData'
 import playerData from './data/playerData'
 
+// helper function
+import { preparePlayerData, addWinsToPlayers} from './helpers/playerHelpers';
+
 function App() {
+  // console.log('preparePlayerData', preparePlayerData(playerData))
+  const playerDataArray = preparePlayerData(playerData);
+  // console.log('playerDataArray from PlayerList', playerDataArray);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  // console.log(playerDataArray[0]);
+  const playerOne = parsedPlayerData[0]
+  console.log('playerOne', playerOne)
   return (
     <div className="App">
       <h1>
         Tourney Matches {" "}
         <span>Where Coding and Tournaments found their Match!</span>
       </h1>
-      <PlayerList matchData={matchData} playerData={playerData}/>
+      <PlayerList playerOne={playerOne}/>
       <MatchList matchData={matchData}/>
     </div>
   );
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,21 +2,24 @@ import React from 'react'
 import Player from './Player'
 // import matchData from '../data/matchData'
 // import playerData from '../data/playerData'
-import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers'
+// import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers'
 
-// console.log("wru?", preparePlayerData(playerData))
+
 function PlayerList(props) {
+  console.log('props from PlayerList', props);
+  console.log('props.playerOne.gamerTag', props.playerOne.gamerTag)
   // console.log(props.matchData)
   // console.log(props.playerData);
-  const playerDataArray = preparePlayerData(props.playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, props.matchData);
+  // const playerDataArray = preparePlayerData(props.playerData);
+  // console.log('playerDataArray from PlayerList', playerDataArray);
+  // const parsedPlayerData = addWinsToPlayers(playerDataArray, props.matchData);
   // console.log(playerDataArray[0]);
-  const playerOne = parsedPlayerData[0]
+  // const playerOne = parsedPlayerData[0]
   // console.log(playerOne)
   return(
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player firstName={playerOne.firstName} gamerTag = {playerOne.gamerTag} lastName={playerOne.lastName} wins={playerOne.wins} />
+      <Player firstName={props.playerOne.firstName} gamerTag = {props.playerOne.gamerTag} lastName={props.playerOne.lastName} wins={props.playerOne.wins} />
     </section>
   )
 }
