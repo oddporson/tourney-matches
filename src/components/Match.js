d5058f7bded1a57265f055593c56b9718c877c01
Porson <porsonlee@gmail.com>
2021-11-23 22:49:11 -0800

    used short circuiting instead of ternary

--- src/components/Match.js
+++ src/components/Match.js
@@ -7,9 +7,8 @@ function Match (props) {
   return (
     <article className="Match">
        <h1>{players[0]} <span>vs</span> {players[1]}</h1>
-      {{winner} ? <h2>{winner} is the winner by {scoreDifference}!</h2> : <h2>No winners yet!</h2>}
-        {/* {winner && <h2>{winner} is the winner by {scoreDifference}!</h2>}
-        {!winner && <h2>No winners yet!</h2>} */}
+        {winner && <h2>{winner} is the winner by {scoreDifference}!</h2>}
+        {!winner && <h2>No winners yet!</h2>}
   </article>
   )
 }
