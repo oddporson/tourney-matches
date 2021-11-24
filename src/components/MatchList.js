1d8c7c7c1fd55c99c7efc4c6c31f47d4252355f3
Porson <porsonlee@gmail.com>
2021-11-23 22:46:03 -0800

    removed comments

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -3,16 +3,13 @@ import Match from './Match'
 
 
 function MatchList(props) {
-  console.log('props', props)
   const { matchData } = props
-  console.log('matchData', matchData)
-  console.log('props.matchData', props.matchData);
-  const oneMatch = props.matchData[0]
+  const oneMatch = matchData[0]
   return(
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
       {/* Matches will be shown here */}
-      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
+      <Match {...oneMatch} />
     </section>
   )
 }
