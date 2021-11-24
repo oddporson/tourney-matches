dbc8dd50198e9d8c64b62400e9a1927ab0ebd25d
Porson <porsonlee@gmail.com>
2021-11-23 23:09:11 -0800

    used map function to iterate players object to render

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import MatchList from './components/MatchList'
 
 // data
 import matchData from './data/matchData'
-import playerData from './data/playerData'
+// import playerData from './data/playerData'
 
 // helper function
-import { preparePlayerData, addWinsToPlayers} from './helpers/playerHelpers';
+// import { preparePlayerData, addWinsToPlayers} from './helpers/playerHelpers';
 
 function App() {
-  const playerDataArray = preparePlayerData(playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
-  const playerOne = parsedPlayerData[0]
+  // const playerDataArray = preparePlayerData(playerData);
+  // const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  // const onePlayer = parsedPlayerData
   return (
     <div className="App">
       <h1>
         Tourney Matches {" "}
         <span>Where Coding and Tournaments found their Match!</span>
       </h1>
-      <PlayerList playerOne={playerOne}/>
+      <PlayerList/>
       <MatchList matchData={matchData}/>
     </div>
   );
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Player from './Player'
 
+// data
+import matchData from '../data/matchData'
+import playerData from '../data/playerData'
+
+// helper function
+import { preparePlayerData, addWinsToPlayers} from '../helpers/playerHelpers';
+
 
 function PlayerList(props) {
+  const preparedPlayerData = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(preparedPlayerData, matchData);
+  const parsedPlayers = parsedPlayerData.map(player => <Player key={player.gamerTag} {...player}/>)
+
   return(
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player firstName={props.playerOne.firstName} gamerTag = {props.playerOne.gamerTag} lastName={props.playerOne.lastName} wins={props.playerOne.wins} />
+      {parsedPlayers}
     </section>
   )
 }
