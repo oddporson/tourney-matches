e1467cbb64cc591ce20ce05739aedcfefd053cee
Porson <porsonlee@gmail.com>
2021-11-23 22:51:41 -0800

    fixed console complaint about missing a key

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -5,7 +5,7 @@ import Match from './Match'
 function MatchList(props) {
   const { matchData } = props
   // const oneMatch = matchData[0]
-  const parsedMatches = matchData.map(match => <Match {...match}/>)
+  const parsedMatches = matchData.map(match => <Match key={match.matchNumber} {...match}/>)
   return(
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
